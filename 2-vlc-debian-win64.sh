580500ef06ce31919c17e77acba97bdd8d4142e0
VideoLAN Buildbot <buildbot@videolan.org>
2018-01-11 13:14:56 -0500

    ming-264

--- 2-vlc-debian-win64.sh
+++ 2-vlc-debian-win64.sh
@@ -23,20 +23,26 @@ MPFR_VERSION=3.1.6
 GMP_VERSION=6.1.1
 MPC_VERSION=1.0.3
 
+mkdir /Downloads
+
 rm -rf /build
-mkdir /build
+mkdir /build/
 cp -rf patches /build
 cd /build
 mkdir $TOOLCHAIN_PREFIX
 mkdir $MINGW_PREFIX
 ln -s $MINGW_PREFIX $TOOLCHAIN_PREFIX/mingw
-wget http://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.bz2
-wget ftp://ftp.uvsq.fr/pub/gcc/releases/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.xz
+if [ ! -f /Downloads/binutils-$BINUTILS_VERSION.tar.bz2 ]; then
+	wget --directory-prefix=/Downloads http://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.bz2
+fi
+if [ ! -f /Downloads/gcc-$GCC_VERSION.tar.xz ]; then
+	wget --directory-prefix=/Downloads ftp://ftp.uvsq.fr/pub/gcc/releases/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.xz
+fi
 git config --global user.name "VideoLAN Buildbot"
 git config --global user.email buildbot@videolan.org
-git clone --depth=1 --branch v5.0.3 git://git.code.sf.net/p/mingw-w64/mingw-w64
-tar xf gcc-$GCC_VERSION.tar.xz
-tar xf binutils-$BINUTILS_VERSION.tar.bz2
+git clone --depth=1 git://git.code.sf.net/p/mingw-w64/mingw-w64
+tar xf /Downloads/gcc-$GCC_VERSION.tar.xz
+tar xf /Downloads/binutils-$BINUTILS_VERSION.tar.bz2
 cd binutils-$BINUTILS_VERSION
 mkdir build
 cd build
@@ -51,12 +57,18 @@ cd build
 ../configure --prefix=$MINGW_PREFIX --host=$TARGET_TUPLE --enable-secure-api
 make install
 cd /build
-wget -q http://www.mpfr.org/mpfr-$MPFR_VERSION/mpfr-$MPFR_VERSION.tar.gz
-wget -q https://gmplib.org/download/gmp/gmp-$GMP_VERSION.tar.xz
-wget -q ftp://ftp.gnu.org/gnu/mpc/mpc-$MPC_VERSION.tar.gz
-tar xf mpfr-$MPFR_VERSION.tar.gz || { echo "Download mpfr-$MPFR_VERSION" ; exit 1; }
-tar xf gmp-$GMP_VERSION.tar.xz || { echo "Download gmp-$GMP_VERSION" ; exit 1; }
-tar xf mpc-$MPC_VERSION.tar.gz || { echo "Download mpc-$MPC_VERSION" ; exit 1; }
+if [ ! -f /Downloads/mpfr-$MPFR_VERSION.tar.gz ]; then
+	wget --directory-prefix=/Downloads -q http://www.mpfr.org/mpfr-$MPFR_VERSION/mpfr-$MPFR_VERSION.tar.gz
+fi
+if [ ! -f /Downloads/gmp-$GMP_VERSION.tar.xz ]; then
+	wget --directory-prefix=/Downloads -q https://gmplib.org/download/gmp/gmp-$GMP_VERSION.tar.xz
+fi
+if [ ! -f /Downloads/mpc-$MPC_VERSION.tar.gz ]; then
+	wget --directory-prefix=/Downloads -q ftp://ftp.gnu.org/gnu/mpc/mpc-$MPC_VERSION.tar.gz
+fi
+tar xf /Downloads/mpfr-$MPFR_VERSION.tar.gz || { echo "Download mpfr-$MPFR_VERSION" ; exit 1; }
+tar xf /Downloads/gmp-$GMP_VERSION.tar.xz || { echo "Download gmp-$GMP_VERSION" ; exit 1; }
+tar xf /Downloads/mpc-$MPC_VERSION.tar.gz || { echo "Download mpc-$MPC_VERSION" ; exit 1; }
 ln -s /build/mpfr-$MPFR_VERSION gcc-$GCC_VERSION/mpfr
 ln -s /build/gmp-$GMP_VERSION gcc-$GCC_VERSION/gmp
 ln -s /build/mpc-$MPC_VERSION gcc-$GCC_VERSION/mpc
@@ -65,19 +77,23 @@ cd gcc-$GCC_VERSION
 mkdir build
 cd build
 ../configure --prefix=$TOOLCHAIN_PREFIX --target=$TARGET_TUPLE --enable-languages=c,c++ --enable-lto --disable-shared --disable-multilib --enable-sjlj-exceptions
-make all-gcc || { echo 'Make all-gcc failed' ; exit 1; }
-ls
+make -j`nproc` all-gcc || { echo 'Make all-gcc failed' ; exit 1; }
 make install-gcc
 cd /build/mingw-w64/mingw-w64-crt
 mkdir build
 cd build
 ../configure --prefix=$MINGW_PREFIX --host=$TARGET_TUPLE
 make -j`nproc`
-ls
 make install
 cd /build/gcc-$GCC_VERSION/build
 make -j`nproc`
-ls
+make install
+cd /build/mingw-w64/mingw-w64-libraries/winstorecompat
+autoreconf -vif
+mkdir build
+cd build
+../configure --prefix=$MINGW_PREFIX --host=$TARGET_TUPLE
+make -j`nproc`
 make install
 cd /build/mingw-w64/mingw-w64-tools/widl
 mkdir build
